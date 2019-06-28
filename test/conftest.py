dc26508a4c73740ad4a52ba974cac4aa5e88ffc4
Brad Dixon <brad.dixon@carvesystems.com>
2019-06-28 10:00:50 -0400

    Remove unused fixtures from test.

--- test/conftest.py
+++ test/conftest.py
@@ -1,27 +1,6 @@
-import pytest
 from scapy.config import conf
 
 from usbq.usbpcap import USBPcap
 
 # Configure scapy to parse USB
 conf.l2types.register(220, USBPcap)
-
-
-@pytest.fixture
-def proxy_ip():
-    return '10.0.10.90'
-
-
-@pytest.fixture
-def proxy_port():
-    return 64241
-
-
-@pytest.fixture
-def listen_ip():
-    return '10.0.10.1'
-
-
-@pytest.fixture
-def listen_port():
-    return 64240
